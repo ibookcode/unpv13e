12e4c4445092021396beee98a6d24c9460892468
Lazybones3 <caorui452@163.com>
2024-03-30 12:54:08 +0000

    daytimetcpcli

---
+++ HELP.md
@@ -0,0 +1,9 @@
+# UNIX Network Programming, Volume 1 The Sockets Networking API, 3rd Edition
+
+## 1 Introduction
+
+## 1.2 A Simple Daytime Client
+
+intro/daytimetcpcli.c
+
+intro/daytimetcpcliv6.c
--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -10,18 +10,26 @@ main(int argc, char **argv)
 	if (argc != 2)
 		err_quit("usage: a.out <IPaddress>");
 
+	// 1.Create TCP socket
 	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
 
+	// 2.Specify server’s IP address and port
+	// Set sockaddr_in structure to 0
 	bzero(&servaddr, sizeof(servaddr));
+	// Set the address family to AF_INET
 	servaddr.sin_family = AF_INET;
+	// Set the port number to 13, the function htons convert the binary port number. (host to network short)
 	servaddr.sin_port   = htons(13);	/* daytime server */
+	// The function inet_pton convert the ASCII command-line argument into the proper format. (presentation to numeric)
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
+	// 3.Establish connection with server
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
+	// 4.Read and display server’s reply
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
@@ -30,5 +38,6 @@ main(int argc, char **argv)
 	if (n < 0)
 		err_sys("read error");
 
+	// 5.Terminate program
 	exit(0);
 }
